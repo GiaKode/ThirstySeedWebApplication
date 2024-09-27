603e07e4698f579a846dcdc2a96160f18c006ebb
Shayla Choque <u20201c144@upc.edu.pe>
2024-09-27 09:10:14 -0500

    general changes

--- src/main.ts
+++ src/main.ts
@@ -7,30 +7,23 @@ import router from './router'
 import PrimeVue from 'primevue/config'
 
 // PrimeVue Material Design Theme
-import Aura from '@primevue/themes/lara'; 
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
+//import Aura from "@primevue/themes/aura";
 
 // PrimeVue Components
-import Button from "primevue/button";
-import InputText from "primevue/inputtext";
-import Checkbox from 'primevue/checkbox';
-
+import ToastService from 'primevue/toastservice'
+import Toast from 'primevue/toast'
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
 // PrimeVue Configuration
-app.use(PrimeVue, {
-    theme: {
-        preset: Aura
-    }
-});
+//app.use(PrimeVue, {ripple: true});
+app.use(PrimeVue)
 
 // Registrar componentes
-app.component('pv-button', Button)
-app.component('pv-input-text', InputText)
-app.component('pv-checkbox', Checkbox)
-
+app.use(ToastService)
+app.component('pv-toast', Toast)
 app.mount('#app')
