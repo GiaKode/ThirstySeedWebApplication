76fbdda381b76588140f5cde0adb7928484103ca
Shayla Choque <u20201c144@upc.edu.pe>
2024-09-26 20:54:22 -0500

    add sidenav and styles to app

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
+import PrimeVue from 'primevue/config'
+
+// PrimeVue Material Design Theme
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+
+// Theme
+
+// PrimeVue Components
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
+// PrimeVue Configuration
+app.use(PrimeVue, { ripple: true })
 app.mount('#app')
